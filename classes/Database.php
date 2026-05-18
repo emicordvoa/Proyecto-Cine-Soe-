bc79dbc169c1c4541c786c2cbbc3ed1c0e6e40c4
emicordvoa <cbbe.danielaemilse.cordova.vi@unifranz.edu.bo>
2026-05-18 04:23:04 -0400

    fix: corregir zona horaria de MySQL para solucionar error del temporizador de 250 mins

--- classes/Database.php
+++ classes/Database.php
@@ -20,6 +20,7 @@ class Database
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '-04:00'",
             ]);
         }
 
